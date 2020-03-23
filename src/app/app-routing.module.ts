be32c78c70ff7fc7d6b03387a805f60fb836258d
JOHN MARK OLESCO <jmolesco@yahoo.com>
2020-03-23 21:56:49 +0800

    initial commit
    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import {HomeComponent} from './modules/home/home.component';
+import {ContactComponent} from   './modules/contact/contact.component';
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  { path:'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './sharedLayout/header/header.component';
+import { FooterComponent } from './sharedLayout/footer/footer.component';
+import { HomeComponent } from './modules/home/home.component';
+import { ContactComponent } from './modules/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
